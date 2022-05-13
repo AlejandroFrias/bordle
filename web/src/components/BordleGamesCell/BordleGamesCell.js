1d77b5237eef39f2ee187b5c14366a692c7dfb30
Alejandro Frias <3598338+AlejandroFrias@users.noreply.github.com>
2022-05-13 10:48:46 -0700

    2/N testing merge conflicts (#2)

--- web/src/components/BordleGamesCell/BordleGamesCell.js
+++ web/src/components/BordleGamesCell/BordleGamesCell.js
@@ -2,7 +2,7 @@ import { Link, routes } from '@redwoodjs/router'
 
 export const QUERY = gql`
   query BordleGamesQuery($playerId: Int!) {
-    bordleGames: myGames(playerId: $playerId) {
+    myBordleGames: myGames(playerId: $playerId) {
       id
       playerOne {
         name
@@ -30,8 +30,8 @@ export const Failure = ({ error }) => (
   <div style={{ color: 'red' }}>Error: {error.message}</div>
 )
 
-export const Success = ({ bordleGames }) => {
-  return bordleGames.map((bordleGame) => (
+export const Success = ({ myBordleGames }) => {
+  return myBordleGames.map((bordleGame) => (
     <article key={bordleGame.id}>
       <header>
         <h2>

6cbe5ea7b7ea537281d7925795417b2409f06be6
Alejandro Frias <alejandro@myriad.com>
2022-05-13 10:43:44 -0700

    3/N testing merge conflicts

--- web/src/components/BordleGamesCell/BordleGamesCell.js
+++ web/src/components/BordleGamesCell/BordleGamesCell.js
@@ -2,7 +2,7 @@ import { Link, routes } from '@redwoodjs/router'
 
 export const QUERY = gql`
   query BordleGamesQuery($playerId: Int!) {
-    myBordleGames: myGames(playerId: $playerId) {
+    myCoolBordleGames: myGames(playerId: $playerId) {
       id
       playerOne {
         name
@@ -30,8 +30,8 @@ export const Failure = ({ error }) => (
   <div style={{ color: 'red' }}>Error: {error.message}</div>
 )
 
-export const Success = ({ myBordleGames }) => {
-  return myBordleGames.map((bordleGame) => (
+export const Success = ({ myCoolBordleGames }) => {
+  return myCoolBordleGames.map((bordleGame) => (
     <article key={bordleGame.id}>
       <header>
         <h2>
